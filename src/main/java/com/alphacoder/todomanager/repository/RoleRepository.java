e49ce8db7fe59e70ff38ab5818da7e818cc3860f
yasithw <yasith.w@eight25media.com>
2023-12-23 20:18:14 +0530

    create login and register endpoints

---
+++ src/main/java/com/alphacoder/todomanager/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.alphacoder.todomanager.repository;
+
+import com.alphacoder.todomanager.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
