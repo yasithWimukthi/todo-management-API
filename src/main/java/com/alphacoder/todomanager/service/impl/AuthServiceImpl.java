5f7e202c52cb199f396557b2feaa124533bd8a6c
yasithw <yasith.w@eight25media.com>
2023-12-24 22:42:03 +0530

    create login and register endpoints

---
+++ src/main/java/com/alphacoder/todomanager/service/impl/AuthServiceImpl.java
@@ -0,0 +1,75 @@
+package com.alphacoder.todomanager.service.impl;
+
+import com.alphacoder.todomanager.dto.LoginDto;
+import com.alphacoder.todomanager.dto.RegisterDto;
+import com.alphacoder.todomanager.entity.Role;
+import com.alphacoder.todomanager.entity.User;
+import com.alphacoder.todomanager.exception.TodoAPIException;
+import com.alphacoder.todomanager.repository.RoleRepository;
+import com.alphacoder.todomanager.repository.UserRepository;
+import com.alphacoder.todomanager.service.AuthService;
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public String register(RegisterDto registerDto) {
+
+        // check username is already exists in database
+        if(userRepository.existsByUsername(registerDto.getUsername())){
+            throw new TodoAPIException(HttpStatus.BAD_REQUEST, "Username already exists!");
+        }
+
+        // check email is already exists in database
+        if(userRepository.existsByEmail(registerDto.getEmail())){
+            throw new TodoAPIException(HttpStatus.BAD_REQUEST, "Email is already exists!.");
+        }
+
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        roles.add(userRole);
+
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
+        return "User Registered Successfully!.";
+    }
+
+    @Override
+    public String login(LoginDto loginDto) {
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(),
+                loginDto.getPassword()
+        ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return "User logged-in successfully!.";
+    }
+}
